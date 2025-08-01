43407e580d0c827c4fd3be0094a0d40910727ef2
AchrafELGhazi <a.elghazi@aui.ma>
2025-08-01 02:18:04 +0100

    feat: add docker compose config

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,24 +6,20 @@ import {
   errorHandler,
 } from './middleware/errorHandler.middleware';
 import { notFoundHandler } from './middleware/notFoundHandler';
+import logger from './config/logger';
 
 const app = express();
 
-// CORS configuration
 //   const allowedOrigins = [
 //     "http://localhost:3000",
-//     "http://localhost:3001",
 //     "https://localhost:3000",
-//     "https://localhost:3001",
 //     env.FRONTEND_URL,
 //   ];
 
-//   // Add production origins
 //   if (env.NODE_ENV === "production") {
 //     // allowedOrigins.push('https://trevo.ma');
 //   }
 
-// Middleware
 app.use(
   helmet({
     crossOriginResourcePolicy: { policy: 'cross-origin' },
@@ -42,47 +38,29 @@ app.use(morgan('combined'));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
-//     app.use(
-//       expressWinston.logger({
-//         winstonInstance: logger,
-//         meta: true,
-//         msg: 'HTTP {{req.method}} {{req.url}}',
-//         expressFormat: true,
-//         colorize: false,
-//         ignoreRoute: req => {
-//           return req.url === '/health' || req.url === '/';
-//         },
-//       })
-// );
 
 // Routes
 app.get('/', (req, res) => {
   res.json({ message: 'Auth API is running!' });
 });
 
-//   app.get('/health', (_req, res) => {
-//     res.status(200).json({
-//       status: 'OK',
-//       timestamp: new Date().toISOString(),
-//       uptime: process.uptime(),
-//       environment: env.NODE_ENV,
-//       version: env.API_VERSION,
-//     });
-//   });
+  // app.get('/health', (_req, res) => {
+  //   res.status(200).json({
+  //     status: 'OK',
+  //     timestamp: new Date().toISOString(),
+  //     uptime: process.uptime(),
+  //     environment: env.NODE_ENV,
+  //     version: env.API_VERSION,
+  //   });
+  // });
 
-//   const apiPrefix = `/api/${env.API_VERSION}`;
+  // const apiPrefix = `/api/${env.API_VERSION}`;
 //   app.use(apiPrefix, apiRouter);
 
 app.use(notFoundHandler);
 
 app.use(errorHandler);
 
-//    app.use(
-//      expressWinston.errorLogger({
-//        winstonInstance: logger,
-//        meta: true,
-//        msg: 'HTTP {{req.method}} {{req.url}} - {{err.message}}',
-//      })
-//    );
+
 
 export default app;
