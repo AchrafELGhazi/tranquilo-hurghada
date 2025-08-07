118f91d916b1f6db134fc07c4066acb92e6b5da2
AchrafElGhazi <a.elghazi@aui.ma>
2025-08-07 02:20:12 +0100

    fix(build): fix build errors for deployment

--- web/src/pages/public/Services.tsx
+++ web/src/pages/public/Services.tsx
@@ -1,7 +1,7 @@
-import { useTranslation } from 'react-i18next';
+// import { useTranslation } from 'react-i18next';
 
 export const Services: React.FC = () => {
-      const { t } = useTranslation();
+    //   const { t } = useTranslation();
 
       return (
             <div className='max-w-2xl mx-auto space-y-8'>
